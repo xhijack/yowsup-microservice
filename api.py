40d43f037c2d4533717ce228b5f04513cdd02c5f
Ramdani Muchsin <ramdanimuchsin@Ramdanis-MacBook-Air.local>
2018-03-19 23:08:00 +0700

    new update

--- api.py
+++ api.py
@@ -11,6 +11,7 @@ Swagger(app)
 # CONFIG = {'AMQP_URI': "pyamqp://guest:guest@localhost"}
 CONFIG = {'AMQP_URI': "amqp://ntjuoyrh:S5mrn6IUnwfzrUaqvPsJ-K60FA9CuFog@skunk.rmq.cloudamqp.com/ntjuoyrh"}
 
+
 @app.route('/send', methods=['POST'])
 @swag_from('docs/send.yml')
 def send():
@@ -18,14 +19,15 @@ def send():
     type = request.json.get('type')
     body = request.json.get('body')
     address = request.json.get('address')
-    logger.info('Get message: %s,%s,%s' % (type,body,address))
+    logger.info('Get message: %s,%s,%s' % (type, body, address))
 
     with ClusterRpcProxy(CONFIG) as rpc:
         # asynchronously spawning and email notification
-        rpc.yowsup.send(type,body,address)
+        rpc.yowsup.send(type, body, address)
 
     msg = "The message was sucessfully sended to the queue"
     return msg, 200
 
+
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80)
